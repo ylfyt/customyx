234a7b4822e3a203f8d85c0b2a479d93662148ff
yudialfayat <ayudi3521@gmail.com>
2022-02-06 21:17:43 +0700

    Setup Tailwind CSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1 class="text-3xl font-bold underline text-red-500">Bismillah Jadi Duit!!!</h1>
+</div>
