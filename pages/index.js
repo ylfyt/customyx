f8b4dcb44e50f945504995f0d029370a304ffd66
yudialfayat <ayudi3521@gmail.com>
2022-02-06 14:26:28 +0700

    Init Nextjs project with Tailwind css

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Bismillah jadi duid</h1>
+		</div>
+	);
+}
