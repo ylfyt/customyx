3ca73cd371645149a51148308d685c9b067127d6
yudialfayat <ayudi3521@gmail.com>
2022-02-13 19:01:52 +0700

    Adding fontawesome and product detail

--- src/routes/product/[slug].svelte
+++ src/routes/product/[slug].svelte
@@ -11,6 +11,9 @@
 </script>
 
 <script>
+	import Icon from 'svelte-fa';
+	import { faStar, faCar } from '@fortawesome/free-solid-svg-icons';
+
 	export let slug;
 
 	const products = [
@@ -45,7 +48,6 @@
 	];
 
 	const product = products.filter((prod) => prod.slug === slug)[0];
-	console.log(product);
 </script>
 
 <div class="container bg-red-200">
@@ -53,13 +55,39 @@
 		<div class="w-2/5">
 			<img src="https://placeimg.com/640/480/any" alt="" />
 			<div class="flex gap-3 mt-[20px]">
-				<img class="w-[100px] h-[100px]" src="https://placeimg.com/640/480/any" alt="" />
-				<img class="w-[100px] h-[100px]" src="https://placeimg.com/640/480/any" alt="" />
-				<img class="w-[100px] h-[100px]" src="https://placeimg.com/640/480/any" alt="" />
+				<img class="w-[75px] h-[75px]" src="https://placeimg.com/640/480/any" alt="" />
+				<img class="w-[75px] h-[75px]" src="https://placeimg.com/640/480/any" alt="" />
+				<img class="w-[75px] h-[75px]" src="https://placeimg.com/640/480/any" alt="" />
 			</div>
 		</div>
 		<div class="ml-[20px] bg-green-200 w-3/5">
-			<h1 class="text-3xl font-semibold">Keyboard Custom Baru</h1>
+			<h1 class="mb-[5px] text-2xl font-semibold">Keyboard Custom Baru</h1>
+			<div class="mb-[5px] flex items-center text-sm font-medium text-textColorSecondary">
+				<p class="mr-1 ">4.7</p>
+				<Icon class="mr-1 text-yellow-400" icon={faStar} />
+				<p>(2.7k ratings)</p>
+				<p class="mr-3 ml-2 text-textColorPrimary font-semibold">|</p>
+				<p>2.7k Sold</p>
+			</div>
+			<div class="bg-secondary text-3xl font-bold p-[10px]">Rp700.000</div>
+			<div>
+				<div class="flex">
+					<div class="w-1/4">Shipping</div>
+					<div class="w-3/4 flex items-start ">
+						<Icon class="mt-[5px]" icon={faCar} />
+						<div class="ml-1 w-full">
+							<div class="flex">
+								<div class="w-[50px] ">To</div>
+								<div class="font-bold">Kota Bandung</div>
+							</div>
+							<div class="flex">
+								<div class="w-[50px] ">Fee</div>
+								<div class="font-semibold">Rp0 - Rp20.000</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
