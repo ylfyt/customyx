38a4f0918a57ab49f2edb2b52c9b4e9f097f7da9
ariaberlian <aria.berlian11@gmail.com>
2022-02-17 12:17:29 +0700

    ongoing order beress

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -22,9 +22,9 @@
                 </div>
 
                 <div class="flex flex-row justify-end text-2xl relative bottom-5 right-3">
-                    <button class="mr-2 bg-white rounded-full px-3">-</button>
+                    <button class="mr-2 bg-white rounded-full px-3 shadow-sm">-</button>
                     <h1 class="mr-2">2</h1>
-                    <button class="bg-white rounded-full px-2">+</button>
+                    <button class="bg-white rounded-full px-2 shadow-sm">+</button>
                     
                 </div>
 			</div>
@@ -36,7 +36,28 @@
 		</div>
 	</section>
 
-	<section class="ongoing-order flex-1 text-5xl font-bold mb-6 ml-10">
-		<h1>Ongoing Orders</h1>
+	<section class="ongoing-order flex-1">
+		<h1 class=" text-5xl font-bold mb-6 ml-10">Ongoing Orders</h1>
+        		<div class="cart-card w-11/12 bg-blue-500 mx-auto h-48 rounded-lg">
+
+                <div class="flex flex-row h-full relative" style="">
+                    <div
+                        style="background-image: url('/electronic.png'); background-position: center; background-size: contain; background-repeat: no-repeat; background-color: white;"
+                        class="h-3/4 w-1/6 relative left-10 my-auto"
+                    />
+                    <div class="desc relative left-12 my-auto">
+                        <h1 class="text-lg">Pintu Kemana saja</h1>
+                        <p class="text-sm mb-1 opacity-70">by DoraHandMade</p>
+                        <p class="mb-1"><b>Rp27.000</b></p>
+                        <p></p>
+                        <p class="font-semibold">Status : Barang sedang dibuat</p>
+                    </div>
+                    <div class="more-detail ml-40">
+                        <button class="font-bold bg-white border-2 px-2 py-1 rounded-xl shadow-sm mt-36">More Detail</button>
+                    </div>
+                </div>
+
+			</div>
+
 	</section>
 </div>
