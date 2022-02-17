4a6484f278d0f5877f1c218ff73cbd57d86685a1
yudialfayat <ayudi3521@gmail.com>
2022-02-17 23:42:50 +0700

    Fix null bug

--- src/routes/product/[slug].svelte
+++ src/routes/product/[slug].svelte
@@ -22,14 +22,18 @@
 	import ProductDescription from '../../components/ProductDescription.svelte';
 
 	export let product;
-	console.log(product);
 	let quantity = 1;
-	let price = product.price;
+	let price = 0;
+	if (product !== null) {
+		price == product.price;
+	}
 	let choice = [];
-	if (typeof product.type !== 'undefined') {
-		product.type.variants.forEach((element) => {
-			choice.push('');
-		});
+	if (product !== null) {
+		if (typeof product.type !== 'undefined') {
+			product.type.variants.forEach((element) => {
+				choice.push('');
+			});
+		}
 	}
 
 	const updateChoice = (idx, co) => {
@@ -39,19 +43,22 @@
 	$: choice && updatePrice();
 
 	const updatePrice = () => {
-		let temp = product.price;
-		if (product.type) {
-			product.type.priceMapping.forEach((el) => {
-				for (let i = 0; i < el.choice.length; i++) {
-					if (el.choice[i] === 'any') {
-						if (choice[i] !== '') continue;
+		let temp = 0;
+		if (product !== null) {
+			temp = product.price;
+			if (product.type) {
+				product.type.priceMapping.forEach((el) => {
+					for (let i = 0; i < el.choice.length; i++) {
+						if (el.choice[i] === 'any') {
+							if (choice[i] !== '') continue;
+						}
+						if (choice[i] !== el.choice[i]) {
+							return;
+						}
 					}
-					if (choice[i] !== el.choice[i]) {
-						return;
-					}
-				}
-				temp = el.price;
-			});
+					temp = el.price;
+				});
+			}
 		}
 
 		price = temp;
@@ -71,103 +78,107 @@
 	};
 </script>
 
-<div class="w-full flex items-center flex-col gap-[20px]">
-	<div class="container flex">
-		<div class="w-2/5">
-			<ProductImagesCarousel images={product.images} />
-		</div>
-		<div class="ml-[20px] w-3/5">
-			<h1 class="mb-[5px] text-2xl font-bold">{product.name}</h1>
-			<div class="mb-[5px] flex items-center text-sm font-medium text-primary">
-				<p class="mr-1 ">{product.rating}</p>
-				<Icon class="mr-1 text-yellow-400" icon={faStar} />
-				<p>
-					({product.numRating > 1100
-						? `${Math.round((product.numRating / 1000) * 100) / 100}k`
-						: product.numRating} ratings)
-				</p>
-				<p class="mr-3 ml-2 text-textColorPrimary font-semibold">|</p>
-				<p>
-					{product.sold > 1100 ? `${Math.round((product.sold / 1000) * 100) / 100}k` : product.sold}
-					Sold
-				</p>
+{#if product !== null}
+	<div class="w-full flex items-center flex-col gap-[20px]">
+		<div class="container flex">
+			<div class="w-2/5">
+				<ProductImagesCarousel images={product.images} />
 			</div>
+			<div class="ml-[20px] w-3/5">
+				<h1 class="mb-[5px] text-2xl font-bold">{product.name}</h1>
+				<div class="mb-[5px] flex items-center text-sm font-medium text-primary">
+					<p class="mr-1 ">{product.rating}</p>
+					<Icon class="mr-1 text-yellow-400" icon={faStar} />
+					<p>
+						({product.numRating > 1100
+							? `${Math.round((product.numRating / 1000) * 100) / 100}k`
+							: product.numRating} ratings)
+					</p>
+					<p class="mr-3 ml-2 text-textColorPrimary font-semibold">|</p>
+					<p>
+						{product.sold > 1100
+							? `${Math.round((product.sold / 1000) * 100) / 100}k`
+							: product.sold}
+						Sold
+					</p>
+				</div>
 
-			<div>
-				<div class="flex mt-[20px]">
-					<div class="w-1/4">Shipping</div>
-					<div class="w-3/4 flex items-start ">
-						<div class="ml-1 w-full">
-							<div class="flex">
-								<div class="w-[50px] ">From</div>
-								<div class="font-medium">{product.address.city}</div>
-							</div>
-							<div class="flex">
-								<div class="w-[50px] ">To</div>
-								<div class="font-bold">Kota Bandung</div>
-							</div>
-							<div class="flex">
-								<div class="w-[50px] ">Fee</div>
-								<div class="font-medium">Rp0 - Rp20.000</div>
+				<div>
+					<div class="flex mt-[20px]">
+						<div class="w-1/4">Shipping</div>
+						<div class="w-3/4 flex items-start ">
+							<div class="ml-1 w-full">
+								<div class="flex">
+									<div class="w-[50px] ">From</div>
+									<div class="font-medium">{product.address.city}</div>
+								</div>
+								<div class="flex">
+									<div class="w-[50px] ">To</div>
+									<div class="font-bold">Kota Bandung</div>
+								</div>
+								<div class="flex">
+									<div class="w-[50px] ">Fee</div>
+									<div class="font-medium">Rp0 - Rp20.000</div>
+								</div>
 							</div>
 						</div>
 					</div>
+					{#if product.type}
+						{#each product.type.variants as variant, i}
+							<VariantField {variant} {updateChoice} idx={i} />
+						{/each}
+					{/if}
 				</div>
-				{#if product.type}
-					{#each product.type.variants as variant, i}
-						<VariantField {variant} {updateChoice} idx={i} />
-					{/each}
-				{/if}
-			</div>
-			<!-- Quantity &  Add To Cart-->
-			<div class="flex mt-[20px]">
-				<div class="w-1/4">Quantity</div>
-				<div class="w-3/4 ">
-					<div class="flex items-center gap-2">
-						<button
-							class="btn text-xl font-bold border-[1px] w-[50px]"
-							on:click={() => updateQuantity(-1)}>-</button
-						>
-						<input
-							type="number"
-							class="bg-secondary px-2 py-3 w-[100px] shadow-sm rounded-md text-center text-xl font-bold"
-							bind:value={quantity}
-							min="1"
-							on:change={updateTest}
-						/>
+				<!-- Quantity &  Add To Cart-->
+				<div class="flex mt-[20px]">
+					<div class="w-1/4">Quantity</div>
+					<div class="w-3/4 ">
+						<div class="flex items-center gap-2">
+							<button
+								class="btn text-xl font-bold border-[1px] w-[50px]"
+								on:click={() => updateQuantity(-1)}>-</button
+							>
+							<input
+								type="number"
+								class="bg-secondary px-2 py-3 w-[100px] shadow-sm rounded-md text-center text-xl font-bold"
+								bind:value={quantity}
+								min="1"
+								on:change={updateTest}
+							/>
 
-						<button
-							class="btn text-xl font-bold border-[1px] w-[50px]"
-							on:click={() => updateQuantity(1)}>+</button
-						>
+							<button
+								class="btn text-xl font-bold border-[1px] w-[50px]"
+								on:click={() => updateQuantity(1)}>+</button
+							>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="flex mt-[20px] bg-secondary py-[10px]">
-				<div class="w-1/4 " />
-				<div class="w-3/4 flex items-center">
-					<button type="submit" class="btn bg-primary text-base font-medium">Add to Cart</button>
-					<div class=" text-3xl font-bold ml-[30px]">
-						Rp{(price * quantity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+				<div class="flex mt-[20px] bg-secondary py-[10px]">
+					<div class="w-1/4 " />
+					<div class="w-3/4 flex items-center">
+						<button type="submit" class="btn bg-primary text-base font-medium">Add to Cart</button>
+						<div class=" text-3xl font-bold ml-[30px]">
+							Rp{(price * quantity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="container">
-		<hr class=" mb-[10px]" />
-		<StoreDetail store={product.store} />
-		<hr class=" mt-[10px]" />
-	</div>
+		<div class="container">
+			<hr class=" mb-[10px]" />
+			<StoreDetail store={product.store} />
+			<hr class=" mt-[10px]" />
+		</div>
 
-	<div class="container bg-secondary">
-		<ProductSpec specifications={product.description.specifications} />
+		<div class="container bg-secondary">
+			<ProductSpec specifications={product.description.specifications} />
+		</div>
+		<div class="container">
+			<ProductDescription description={product.description.text} />
+		</div>
 	</div>
-	<div class="container">
-		<ProductDescription description={product.description.text} />
-	</div>
-</div>
+{/if}
 
 <style>
 	input::-webkit-outer-spin-button,
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,7 @@
 export default {
 	findProductBySlug: (slug) => {
 		const prds = products.filter((product) => product.slug === slug);
-		if (prds.length !== 1) {
+		if (prds.length === 0) {
 			return null;
 		} else {
 			return prds[0];
@@ -218,6 +218,408 @@ const products = [
                 <br/>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
                 <br/>
+                <br/>
+            `
+		}
+	},
+	{
+		id: 3,
+		name: 'Custom Gitar',
+		slug: 'custom-gitar-31230213212',
+		storeId: 2,
+		sold: 1200,
+		price: 750000,
+		rating: 4.6,
+		numRating: 990,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		images: [
+			{
+				url: '/img/guitar0.jpg',
+				caption: 'Guitar 1'
+			},
+			{
+				url: '/img/guitar1.jpg',
+				caption: 'Guitar 2'
+			},
+			{
+				url: '/img/guitar2.jpg',
+				caption: 'Guitar 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+            `
+		}
+	},
+	{
+		id: 1,
+		name: 'Keyboard Mechanical Custom',
+		slug: 'keyboard-mechanical-custom-31230213212',
+		storeId: 2,
+		sold: 4050,
+		price: 525000,
+		rating: 4.9,
+		numRating: 3200,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		type: {
+			variants: [
+				{
+					name: 'Color',
+					options: ['White', 'Black']
+				},
+				{
+					name: 'Switch',
+					options: ['Red', 'Brown', 'Black']
+				}
+			],
+			priceMapping: []
+		},
+		images: [
+			{
+				url: '/img/keyboard0.jpg',
+				caption: 'Photo 1'
+			},
+			{
+				url: '/img/keyboard1.jpg',
+				caption: 'Photo 2'
+			},
+			{
+				url: '/img/keyboard2.jpg',
+				caption: 'Photo 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				},
+				{
+					title: 'Tegangan',
+					value: '12v'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?ss
+                <br/>
+            `
+		}
+	},
+	{
+		id: 1,
+		name: 'Keyboard Mechanical Custom',
+		slug: 'keyboard-mechanical-custom-31230213212',
+		storeId: 2,
+		sold: 4050,
+		price: 525000,
+		rating: 4.9,
+		numRating: 3200,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		type: {
+			variants: [
+				{
+					name: 'Color',
+					options: ['White', 'Black']
+				},
+				{
+					name: 'Switch',
+					options: ['Red', 'Brown', 'Black']
+				}
+			],
+			priceMapping: []
+		},
+		images: [
+			{
+				url: '/img/keyboard0.jpg',
+				caption: 'Photo 1'
+			},
+			{
+				url: '/img/keyboard1.jpg',
+				caption: 'Photo 2'
+			},
+			{
+				url: '/img/keyboard2.jpg',
+				caption: 'Photo 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				},
+				{
+					title: 'Tegangan',
+					value: '12v'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?ss
+                <br/>
+            `
+		}
+	},
+	{
+		id: 3,
+		name: 'Custom Gitar',
+		slug: 'custom-gitar-31230213212',
+		storeId: 2,
+		sold: 1200,
+		price: 750000,
+		rating: 4.6,
+		numRating: 990,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		images: [
+			{
+				url: '/img/guitar0.jpg',
+				caption: 'Guitar 1'
+			},
+			{
+				url: '/img/guitar1.jpg',
+				caption: 'Guitar 2'
+			},
+			{
+				url: '/img/guitar2.jpg',
+				caption: 'Guitar 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+            `
+		}
+	},
+	{
+		id: 3,
+		name: 'Custom Gitar',
+		slug: 'custom-gitar-31230213212',
+		storeId: 2,
+		sold: 1200,
+		price: 750000,
+		rating: 4.6,
+		numRating: 990,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		images: [
+			{
+				url: '/img/guitar0.jpg',
+				caption: 'Guitar 1'
+			},
+			{
+				url: '/img/guitar1.jpg',
+				caption: 'Guitar 2'
+			},
+			{
+				url: '/img/guitar2.jpg',
+				caption: 'Guitar 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+            `
+		}
+	},
+	{
+		id: 1,
+		name: 'Keyboard Mechanical Custom',
+		slug: 'keyboard-mechanical-custom-31230213212',
+		storeId: 2,
+		sold: 4050,
+		price: 525000,
+		rating: 4.9,
+		numRating: 3200,
+		address: {
+			city: 'Kota Surabaya'
+		},
+		store: {
+			name: 'Toko Jaya',
+			image: '/img/keyboard1.jpg',
+			sold: 23000,
+			rating: 4.5,
+			numRating: 8750,
+			lastActive: 450,
+			note: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere vero libero veniam quisquam dolor voluptatibus illum fugiat magnam soluta. Neque odio alias possimus, necessitatibus'
+		},
+		type: {
+			variants: [
+				{
+					name: 'Color',
+					options: ['White', 'Black']
+				},
+				{
+					name: 'Switch',
+					options: ['Red', 'Brown', 'Black']
+				}
+			],
+			priceMapping: []
+		},
+		images: [
+			{
+				url: '/img/keyboard0.jpg',
+				caption: 'Photo 1'
+			},
+			{
+				url: '/img/keyboard1.jpg',
+				caption: 'Photo 2'
+			},
+			{
+				url: '/img/keyboard2.jpg',
+				caption: 'Photo 3'
+			}
+		],
+		description: {
+			specifications: [
+				{
+					title: 'Kondisi',
+					value: 'Baru'
+				},
+				{
+					title: 'Garansi',
+					value: '1 Tahun'
+				},
+				{
+					title: 'Tegangan',
+					value: '12v'
+				}
+			],
+			text: `
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit, vero est, reiciendis repellat, accusantium officia illo voluptatibus eum repellendus corporis iure cumque sunt molestias quisquam qui aliquid rerum dolorem?ss
                 <br/>
             `
 		}
